b517ec885ac373d11d672273e90a142f0bd60937
mediumjames <JamesCate@mediumjames.com>
2018-10-24 12:53:07 -0400

    Picker and text input views done, working on multiple choice, resetting package.json

--- UTKMentorApp/screens/MenteeAppScreen.js
+++ UTKMentorApp/screens/MenteeAppScreen.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
   StyleSheet,
   TouchableOpacity,
   Button,
   ScrollView,
-  Picker
 } from 'react-native';
-import ModalSelector from 'react-native-modal-selector'
+import ModalSelector from 'react-native-modal-selector';
+import MultipleChoice from 'react-native-multiple-choice';
 import Amplify, { Auth, API } from 'aws-amplify';
 
 export default class MenteeApplication extends Component {
@@ -16,10 +17,31 @@ export default class MenteeApplication extends Component {
     class_year: 'Freshman',
     gender: '',
     major: '',
+    minors: '',
+    high_GPA: '',
     grad_interested: '',
     grad_school: '',
     research: '',
-    honors: ''
+    honors: '',
+    interests: {'cooking/baking': false,
+                'coops/internships': false,
+                'crafting/diy/making': false,
+                'entrepreneurship/business': false,
+                'fitness': false,
+                'hiking/backpacking': false,
+                'movies/tv': false,
+                'music': false,
+                'politics': false,
+                'research': false,
+                'social media': false,
+                'sports': false,
+                'sustainability': false,
+                'travel': false,
+                'video games': false
+              },
+    weekend: '',
+    job: '',
+    agree: ''
   }
 
   setStateHelper(key, value) {
@@ -86,8 +108,92 @@ export default class MenteeApplication extends Component {
         <Text>Class For Year of 2018-2019</Text>
         <ModalSelector
           data={class_years}
-          initValue="Class Year"
+          initValue="Select"
           onChange={(option) => this.setStateHelper('class_year', option.key)} />
+
+        <Text>Gender</Text>
+        <ModalSelector
+          data={genders}
+          initValue="Select"
+          onChange={(option) => this.setStateHelper('gender', option.key)} />
+
+        <Text>Major</Text>
+        <ModalSelector
+          data={majors}
+          initValue="Select"
+          onChange={(option) => this.setStateHelper('major', option.key)} />
+
+        <TextInput
+          onChangeText={value => this.setStateHelper('minors', value)}
+          blurOnSubmit={false}
+          keyboardAppearance='dark'
+          returnKeyType='done'
+          underlineColorAndroid='transparent'
+          placeholder='Minors'
+        />
+
+        <Text>Are you interested in graduate or professional education?</Text>
+        <ModalSelector
+          data={prof_options}
+          initValue="Select"
+          onChange={(option) => this.setStateHelper('grad_interested', option.key)} />
+
+        <Text>What type of postsecondary education?</Text>
+        <ModalSelector
+          data={grad_schools}
+          initValue="Select"
+          onChange={(option) => this.setStateHelper('grad_school', option.key)} />
+
+        <Text>Are you interested in research at UT?</Text>
+        <ModalSelector
+          data={research_involvement}
+          initValue="No"
+          onChange={(option) => this.setStateHelper('research', option.key)} />
+
+        <Text>Are you in an honors program?</Text>
+        <ModalSelector
+          data={in_honors}
+          initValue="No"
+          onChange={(option) => this.setStateHelper('honors', option.key)} />
+
+        <MultipleChoice
+          options={[
+            'Cooking/Baking',
+            'Coops/Internships',
+            'Crafting/DIY/Making',
+            'Entrepreneurship/Business',
+            'Fitness',
+            'Hiking/Backpacking',
+            'Movies/TV',
+            'Music',
+            'Politics',
+            'Research',
+            'Social Media',
+            'Sports',
+            'Sustainability',
+            'Travel',
+            'Video Games'
+          ]}
+        />
+
+        <TextInput
+          onChangeText={value => this.setStateHelper('weekend', value)}
+          blurOnSubmit={false}
+          keyboardAppearance='dark'
+          returnKeyType='done'
+          underlineColorAndroid='transparent'
+          placeholder='Weekend'
+        />
+
+        <TextInput
+          onChangeText={value => this.setStateHelper('job', value)}
+          blurOnSubmit={false}
+          keyboardAppearance='dark'
+          returnKeyType='done'
+          underlineColorAndroid='transparent'
+          placeholder='Dream Job'
+        />
+
       </ScrollView>
     );
   }
